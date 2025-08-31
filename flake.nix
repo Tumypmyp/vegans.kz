2ea915390a51b91fc10360f0d22b994fc9263129
Timur Gainullin <gaynoullin@gmail.com>
2025-08-31 23:31:55 +0500

    add main app

---
+++ flake.nix
@@ -0,0 +1,89 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+
+    dioxus.url = "github:DioxusLabs/dioxus/main";
+    dioxus.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, flake-parts, dioxus, ... } @inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" "aarch64-linux" ];
+
+      perSystem = { self', config, pkgs, lib, system, ... }: let
+        rustToolchain = pkgs.rust-bin.stable.latest.default.override {
+          extensions = [ "rust-src" "rust-analyzer" "clippy" ];
+          targets = [ "wasm32-unknown-unknown" ];
+        };
+        rustBuildInputs = (with pkgs; [ openssl libiconv pkg-config ])
+          ++ lib.optionals pkgs.stdenv.isLinux (with pkgs; [
+            glib gtk3 libsoup_3 webkitgtk_4_1 xdotool
+          ])
+          ++ lib.optionals pkgs.stdenv.isDarwin (with pkgs.darwin.apple_sdk.frameworks; [
+            SystemConfiguration IOKit Carbon WebKit Security Cocoa
+          ]);
+      in
+      {
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ 
+            inputs.rust-overlay.overlays.default 
+          ];
+        };
+        formatter = pkgs.nixfmt-rfc-style;
+
+        packages.default = let
+          cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+          rev = toString (self.shortRev or self.dirtyShortRev or self.lastModified or "unknown");
+        in
+        pkgs.rustPlatform.buildRustPackage {
+          pname = cargoToml.package.name;
+          version = "${cargoToml.package.version}-${rev}";
+          src = ./.;
+          strictDeps = true;
+          buildInputs = rustBuildInputs;
+          nativeBuildInputs = with pkgs; [
+            dioxus.packages.${system}.dioxus-cli
+            rustToolchain
+            rustPlatform.bindgenHook
+            wasm-bindgen-cli_0_2_100
+            binaryen
+          ] ++ rustBuildInputs;
+          
+          buildPhase = ''
+            echo "Running dx bundle --release --web --ssg..."
+            dx bundle --release --web --ssg
+            echo "dx bundle completed. Output should be in 'dist' folder."
+          '';
+
+          installPhase = ''
+            echo "Installing bundled static assets to pubc..."
+            rm -rf public
+            mkdir -p public
+            # Copy all contents of the generated 'dist' directory to the output root
+            cp -r target/dx/$pname/release/web/public public
+            echo "Installation complete."
+          '';
+          cargoLock.lockFile = ./Cargo.lock;
+          meta.mainProgram = "server";
+        };
+
+        devShells.default = pkgs.mkShell rec {
+          name = "dioxus-dev";
+          buildInputs = rustBuildInputs;
+          nativeBuildInputs = with pkgs; [
+            rustToolchain
+            wasm-bindgen-cli_0_2_100
+            dioxus.packages.${system}.dioxus-cli
+            binaryen
+          ];
+          RUST_SRC_PATH="${rustToolchain}/lib/rustlib/src/rust/library";
+          
+          shellHook = ''
+          '';
+        };
+      };
+    };
+}
---
+++ src/main.rs
@@ -0,0 +1,131 @@
+use dioxus::prelude::*;
+
+#[derive(Debug, Clone, Routable, PartialEq)]
+#[rustfmt::skip]
+enum Route {
+    #[route("/")]
+    Home {},
+}
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS }
+        Router::<Route> {}
+    }
+}
+
+
+#[component]
+fn Home() -> Element {
+    rsx! {
+        Hero {}
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    rsx! {
+        div { id: "hero",
+            div { id: "title", "Веган ресурсы Алматы" }
+            div { id: "links",
+                Resource {
+                    src: "https://veganrussian.ru/",
+                    title: "VeganRussian.ru",
+                    description: "Каталог веганских продуктов",
+                }
+                Resource {
+                    src: "https://www.instagram.com/omirfest/",
+                    title: "OmirFest",
+                    platform: SocialMedia::Instagram,
+                }
+                Resource {
+                    src: "https://www.instagram.com/omirconf/",
+                    title: "OmirConf",
+                    platform: SocialMedia::Instagram,
+                }
+                Resource {
+                    src: "https://t.me/veganDvizhAlmaty",
+                    title: "Веган Движ Алматы",
+                    description: "Канал с веган мероприятиями в Алматы",
+                    platform: SocialMedia::Telegram,
+                }
+                Resource {
+                    src: "https://t.me/veganstan",
+                    title: "Веганстан",
+                    description: "Группа для веганов и стремящихся к веганству в Казахстане",
+                    platform: SocialMedia::Telegram,
+                }
+                Resource {
+
+                    src: "",
+                    title: "Vegan🌱Almaty",
+                    description: "Группа веганов Алматы (вход по приглашению участника)",
+                    platform: SocialMedia::Telegram,
+                }
+            }
+        }
+    }
+}
+
+#[derive(Clone, PartialEq)]
+pub enum SocialMedia {
+    Instagram,
+    Telegram,
+}
+
+fn get_icon_for_platform(platform: SocialMedia) -> Element {
+    match platform {
+    SocialMedia::Instagram =>    
+        rsx!{
+            div { class: "icon-holder",
+                img {
+                    src: "https://img.icons8.com/?size=100&id=RhYNENh5cxlS&format=png&color=000000",
+                    // src: "https://img.icons8.com/ios/100/instagram-new--v1.png",
+                    alt: "instagram",
+                    width: "30px",
+                }
+            }
+        },
+    SocialMedia::Telegram =>
+        rsx!{
+            div { class: "icon-holder",
+                img {
+                    src: "https://img.icons8.com/?size=100&id=lUktdBVdL4Kb&format=png&color=000000",
+                    // src: "https://img.icons8.com/?size=100&id=A4uyUh8LBg90&format=png&color=000000",
+                    alt: "instagram",
+                    width: "28px",
+                }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn Resource( title: String, src: String, description: Option<String>, platform: Option<SocialMedia>) -> Element {
+    rsx!{
+        Link { to: src.clone(), new_tab: true,
+            onclick: move |_| {
+            },
+            onclick_only: src == "",
+            
+            div { class: "resource-title",
+                if let Some(p) = platform {
+                    {get_icon_for_platform(p)}
+                }
+                "{title}"
+            }
+            if let Some(d) = description {
+                div { class: "description", "{d}" }
+            }
+        }
+    }
+}
